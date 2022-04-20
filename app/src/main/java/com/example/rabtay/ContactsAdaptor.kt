614d5fe86d394689f4c55924c89160f5a430f134
Farhan Ali <l191236@lhr.nu.edu.pk>
2022-04-20 21:37:31 +0500

    Added contact_item layout

---
+++ app/src/main/java/com/example/rabtay/ContactsAdaptor.kt
@@ -0,0 +1,9 @@
+package com.example.rabtay
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactsAdaptor: RecyclerView.Adapter<ContactsAdaptor.ViewHolder>() {
+    inner class ViewHolder {
+
+    }
+}
